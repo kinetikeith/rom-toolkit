2c659ea0600e50e2f15e8a190e847a6ba7c7060b
Keith Hartman <khman32@protonmail.com>
2022-12-31 11:23:47 -0500

    Updated format function signatures to always return strings

--- src/ui/format.ts
+++ src/ui/format.ts
@@ -1,14 +1,14 @@
-export function asHexRaw(
-  value: number | undefined,
-  width: number = 2
-): string | undefined {
-  if (value === undefined) return undefined;
-  if (Number.isNaN(value)) return undefined;
+export function asHexRaw(value: number | undefined, width: number = 2): string {
+  if (value === undefined) return "Unknown";
+  if (Number.isNaN(value)) return "Unknown";
 
   return value.toString(16).slice(-width).toUpperCase().padStart(width, "0");
 }
 
 export function asHex(value: number | undefined, width: number = 2) {
+  if (value === undefined) return "Unknown";
+  if (Number.isNaN(value)) return "Unknown";
+
   return "0x" + asHexRaw(value, width);
 }
 
@@ -21,8 +21,8 @@ const units: { [index: number]: { name: string; div: number } } = {
   4: { name: "TiB", div: 1024 * 1024 * 1024 * 1024 },
 };
 
-export function asBytes(value: number | undefined): string | undefined {
-  if (value === undefined) return undefined;
+export function asBytes(value: number | undefined): string {
+  if (value === undefined) return "Unknown";
   const unitIndex = Math.floor(Math.log(value) / base1024);
   let unit = units[unitIndex];
 
