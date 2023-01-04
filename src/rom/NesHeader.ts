f8cf8e85a16e7888404410ef30ff904bcaadf7dc
Keith Hartman <khman32@protonmail.com>
2023-01-04 13:07:06 -0500

    Made NES headers editable, corrected header searching issue

--- src/rom/NesHeader.ts
+++ src/rom/NesHeader.ts
@@ -3,6 +3,9 @@ import { sortBy } from "lodash";
 
 import { mod } from "./utils";
 
+import chrSizes from "./data/nesChrSizes.json";
+import prgSizes from "./data/nesPrgSizes.json";
+
 const iNesMagic = "NES\x1a";
 
 export enum Format {
@@ -11,23 +14,39 @@ export enum Format {
   INes2,
 }
 
-enum Encoding {
+export enum Encoding {
   Invalid = -1,
   None,
   Ascii,
   JisX,
 }
 
-enum Mirroring {
+export enum Mirroring {
   None = -1,
   Horizontal,
   Vertical,
 }
 
-enum ChrType {
+export enum ChrType {
   Rom,
   Ram,
 }
+
+const chrSizeMap = new Map<number, number[]>();
+const prgSizeMap = new Map<number, number>();
+
+for (const [key, value] of Object.entries(chrSizes)) {
+  chrSizeMap.set(
+    Number.parseInt(key, 16),
+    value.map((size) => size * 1024)
+  );
+}
+
+for (const [key, value] of Object.entries(prgSizes)) {
+  prgSizeMap.set(Number.parseInt(key, 16), value * 1024);
+}
+
+export { chrSizeMap, prgSizeMap };
 
 const asciiRegex = /[\x20-\x3f\x41-\x5a]+/;
 const jisXRegex = /[\x20-\x7e\xa1-\df]+/;
@@ -40,7 +59,7 @@ function findHeader(buffer: Buffer): NesHeader {
 
   let liPoss = [];
 
-  for (let liEnd = 0xfffa; liEnd < dataBuffer.length; liEnd += 0x1000) {
+  for (let liEnd = 0xfffa; liEnd < dataBuffer.length; liEnd += 0x10000) {
     const liStart = liEnd - 0x1a;
     const testHeader = new NesHeader(buffer, liStart);
     liPoss.push({ loc: liStart, score: testHeader.validity });
@@ -71,8 +90,7 @@ export default class NesHeader {
 
   get validity(): number {
     let score = 0;
-    if (this._buffer.length < 0xfffa) return -1;
-    /* Check if the format is accurate */
+    /* Check if additional format info is present */
     if (this.format === Format.INes) score += 3;
     else if (this.format === Format.INes2) score += 3;
 
@@ -90,11 +108,17 @@ export default class NesHeader {
       else if (matchLen === titleLength - 1) score += 2;
     }
 
+    /* Check that CHR checksum is blank if CHR type is RAM */
+    if (this.chrType === ChrType.Ram) {
+      if (this.chrChecksum === 0) score += 1;
+      else score -= 1;
+    }
+
     /* Verify the checksum */
-    if (this.licenseChecksumCalc === 0) {
-      score += 2;
+    if (this.headerChecksumCalc === 0) {
+      score += 1;
       /* If complement is non-zero and correct, likelihood greater */
-      if (this.licenseComplement !== 0) score += 2;
+      if (this.headerComplement !== 0) score += 3;
     } else score -= 4;
 
     return score;
@@ -124,7 +148,16 @@ export default class NesHeader {
   }
 
   get title(): string {
+    // TODO: Trim padding from beginning of title
     return this._licenseBuffer.toString("ascii", 0x00, 0x10);
+  }
+  set title(value: string) {
+    // TODO: Ensure that space padding is correct treatment of title
+    const trimmed = value.slice(0, 16);
+    const length = trimmed.length;
+    const padded = trimmed.padStart(16, " ");
+    this._licenseBuffer.write(padded, 0x00, 16, "ascii");
+    this._licenseBuffer.writeUInt8(Math.max(0, length - 1), 0x17);
   }
 
   get prgChecksum(): number {
@@ -136,18 +169,43 @@ export default class NesHeader {
   }
 
   get chrCode(): number {
-    return this._licenseBuffer.readUInt8(0x14) & 0b0011;
+    return this._licenseBuffer.readUInt8(0x14) & 0b0111;
+  }
+  set chrCode(value: number) {
+    const prev = this._licenseBuffer.readUInt8(0x14);
+    const bits = value & 0b00000111;
+    const otherMask = 0b11111000;
+    this._licenseBuffer.writeUInt8(bits | (prev & otherMask), 0x14);
+  }
+  get chrSize(): number[] | undefined {
+    return chrSizeMap.get(this.chrCode);
   }
 
   get chrType(): ChrType {
-    const value = (this._licenseBuffer.readUInt8(0x14) & 0b0100) >> 2;
+    const value = (this._licenseBuffer.readUInt8(0x14) & 0b1000) >> 3;
 
     if (value) return ChrType.Rom;
     else return ChrType.Ram;
   }
+  set chrType(value: ChrType) {
+    const prev = this._licenseBuffer.readUInt8(0x14);
+    const bit = value === ChrType.Rom ? 0b1000 : 0b0000;
+    const otherMask = 0b111101111;
+    this._licenseBuffer.writeUInt8(bit | (prev & otherMask), 0x14);
+  }
 
   get prgCode(): number {
-    return (this._licenseBuffer.readUInt8(0x14) & 0b11111000) >> 3;
+    return (this._licenseBuffer.readUInt8(0x14) & 0b11110000) >> 4;
+  }
+  set prgCode(value: number) {
+    const prev = this._licenseBuffer.readUInt8(0x14);
+    const bits = (value << 4) & 0b11110000;
+    const otherMask = 0b00001111;
+    this._licenseBuffer.writeUInt8(bits | (prev & otherMask), 0x14);
+  }
+
+  get prgSize(): number | undefined {
+    return prgSizeMap.get(this.prgCode);
   }
 
   get mapperCode(): number {
@@ -165,7 +223,7 @@ export default class NesHeader {
     const value = this._licenseBuffer.readUInt8(0x16);
     if (value === 0) return Encoding.None;
     else if (value === 1) return Encoding.Ascii;
-    else if (value === 2) return Encoding.Ascii;
+    else if (value === 2) return Encoding.JisX;
 
     return Encoding.Invalid;
   }
@@ -180,11 +238,11 @@ export default class NesHeader {
     return (this._licenseBuffer.readUInt8(0x18) & 0b10000000) >> 7;
   }
 
-  get licenseComplement(): number {
+  get headerComplement(): number {
     return this._licenseBuffer.readUInt8(0x19);
   }
 
-  get licenseChecksumCalc(): number {
+  get headerChecksumCalc(): number {
     let checksum = 0x00;
     const checkBuffer = this._licenseBuffer.subarray(0x12, 0x1a);
 
--- src/ui/editor/NesHeaderEditor.tsx
+++ src/ui/editor/NesHeaderEditor.tsx
@@ -1,34 +1,142 @@
-import { useContext } from "react";
+import { useState, useContext, useMemo } from "react";
 
 import { RomContext } from "../../AppData";
-import NesHeader, { Format } from "../../rom/NesHeader";
+import NesHeader, {
+  Format,
+  ChrType,
+  chrSizeMap,
+  prgSizeMap,
+} from "../../rom/NesHeader";
 
 import { HeaderEntry, HeaderDivider } from "./HeaderEditor";
-import { asHex } from "../format";
+import { asHex, asBytes } from "../format";
+import StringDialog from "../dialog/StringDialog";
+import ChoiceDialog from "../dialog/ChoiceDialog";
 
-const headerFormatLabelMap = new Map([
+enum Field {
+  None,
+  Title,
+  ChrSize,
+  ChrType,
+  PrgSize,
+}
+
+const formatLabelMap = new Map([
   [Format.Raw, "Headerless"],
   [Format.INes, "iNES"],
   [Format.INes2, "NES 2.0"],
 ]);
 
-export default function NesHeaderEditor(props: {}) {
-  const context = useContext(RomContext);
+const chrTypeLabelMap = new Map([
+  [ChrType.Rom, "ROM"],
+  [ChrType.Ram, "RAM"],
+]);
 
-  const header = NesHeader.fromRom(context.buffer);
+const chrSizeLabelMap = new Map<number, string>(
+  [...chrSizeMap.entries()].map(([key, value]) => [
+    key,
+    value.map((n) => asBytes(n)).join(" or "),
+  ])
+);
+
+const prgSizeLabelMap = new Map<number, string>(
+  [...prgSizeMap.entries()].map(([key, value]) => [key, asBytes(value)])
+);
+
+export default function NesHeaderEditor(props: {}) {
+  const [field, setField] = useState<Field>(Field.None);
+  const context = useContext(RomContext);
+  const buffer = context.buffer;
+  const header = useMemo(() => NesHeader.fromRom(buffer), [buffer]);
+
+  const setFieldTo = (value: Field) => () => setField(value);
+  const closeField = () => setField(Field.None);
+
+  const headerChecksumCalc = header.headerChecksumCalc;
+  const headerChecksumColor =
+    headerChecksumCalc === 0 ? "success.main" : "error.main";
 
   return (
     <>
       <HeaderDivider>General</HeaderDivider>
-      <HeaderEntry label="Title">{header.title}</HeaderEntry>
-      <HeaderDivider>Licensing</HeaderDivider>
-      <HeaderEntry label="Location">{asHex(header.licenseLoc, 6)}</HeaderEntry>
-      <HeaderEntry label="Complement">
-        {asHex(header.licenseComplement)}
+      <HeaderEntry label="Title" onEdit={setFieldTo(Field.Title)}>
+        {header.title}
+      </HeaderEntry>
+      <StringDialog
+        title="Edit Title"
+        open={field === Field.Title}
+        value={header.title}
+        maxLength={16}
+        onCancel={closeField}
+        onSubmit={(value) => {
+          header.title = value;
+
+          context.updateBuffer();
+          closeField();
+        }}
+      />
+      <HeaderEntry label="Header Location">
+        {asHex(header.licenseLoc, 6)}
+      </HeaderEntry>
+      <HeaderDivider>Hardware</HeaderDivider>
+      <HeaderEntry label="CHR Size" onEdit={setFieldTo(Field.ChrSize)}>
+        {chrSizeLabelMap.get(header.chrCode) || "Unknown"}
+      </HeaderEntry>
+      <ChoiceDialog
+        title="Edit CHR Size"
+        open={field === Field.ChrSize}
+        value={header.chrCode}
+        optionMap={chrSizeLabelMap}
+        onCancel={closeField}
+        onSubmit={(value) => {
+          header.chrCode = value;
+          context.updateBuffer();
+          closeField();
+        }}
+      />
+      <HeaderEntry label="CHR Type" onEdit={setFieldTo(Field.ChrType)}>
+        {chrTypeLabelMap.get(header.chrType) || "Unknown"}
+      </HeaderEntry>
+      <ChoiceDialog
+        title="Edit CHR Type"
+        open={field === Field.ChrType}
+        value={header.chrType}
+        optionMap={chrTypeLabelMap}
+        onCancel={closeField}
+        onSubmit={(value) => {
+          header.chrType = value;
+          context.updateBuffer();
+          closeField();
+        }}
+      />
+      <HeaderEntry label="PRG Size" onEdit={setFieldTo(Field.PrgSize)}>
+        {prgSizeLabelMap.get(header.prgCode)}
+      </HeaderEntry>
+      <ChoiceDialog
+        title="Edit PRG Size"
+        open={field === Field.PrgSize}
+        value={header.prgCode}
+        optionMap={prgSizeLabelMap}
+        onCancel={closeField}
+        onSubmit={(value) => {
+          header.prgCode = value;
+          context.updateBuffer();
+          closeField();
+        }}
+      />
+      <HeaderDivider>Checksums</HeaderDivider>
+      <HeaderEntry label="Header Complement" color={headerChecksumColor}>
+        {asHex(header.headerComplement)}
+      </HeaderEntry>
+      <HeaderEntry label="CHR Checksum">
+        {asHex(header.chrChecksum, 4)}
+      </HeaderEntry>
+      <HeaderEntry label="PRG Checksum">
+        {asHex(header.prgChecksum, 4)}
       </HeaderEntry>
       <HeaderDivider>Format</HeaderDivider>
       <HeaderEntry label="Type">
-        {headerFormatLabelMap.get(header.format)}
+        {formatLabelMap.get(header.format)}
       </HeaderEntry>
     </>
   );
