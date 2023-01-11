571645a6a0f8fc530e205950698a413ea67a4ca8
Keith Hartman <khman32@protonmail.com>
2023-01-11 13:18:30 -0500

    Added version info to PageFooter

--- src/ui/PageFooter.tsx
+++ src/ui/PageFooter.tsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
 import packageInfo from "../../package.json";
@@ -11,6 +12,8 @@ export default function PageFooter(props: {}) {
       <Link href="https://reactjs.org" color="secondary">
         React
       </Link>
+      <br />
+      <Typography variant="mono2">v{packageInfo.version}</Typography>
       <br />
       <GitHubIcon fontSize="inherit" />{" "}
       <Link color="secondary" href={packageInfo.repository.url}>
